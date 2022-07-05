08206e325eaeb8ca05e580d3f644a0a10a688575
TbcProjects <daniel@thebikeclub.co.uk>
2022-07-06 00:16:24 +0100

    Inital commit

---
+++ hydrogen.config.js
@@ -0,0 +1,18 @@
+import {defineConfig, CookieSessionStorage} from '@shopify/hydrogen/config';
+
+export default defineConfig({
+  shopify: {
+    defaultCountryCode: 'US',
+    defaultLanguageCode: 'EN',
+    storeDomain: 'https://db-dev-testing.myshopify.com/',
+    storefrontToken: '3af9cd45fd0a2a3259e0e6bac3986135',
+    storefrontApiVersion: '2022-07',
+  },
+  session: CookieSessionStorage('__session', {
+    path: '/',
+    httpOnly: true,
+    secure: import.meta.env.PROD,
+    sameSite: 'Strict',
+    maxAge: 60 * 60 * 24 * 30,
+  }),
+});
